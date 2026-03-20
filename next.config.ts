24fae4620a006cc7d342633a6759139d275daa48
AbuArwa001 <khalfanathman12@yahoo.com>
2026-03-20 22:58:18 +0300

    feat: Add supkem.co.ke, S3, and localhost to Next.js remote image patterns.

--- next.config.ts
+++ next.config.ts
@@ -16,6 +16,18 @@ const nextConfig: NextConfig = {
         protocol: 'https',
         hostname: 'supkem-drf.onrender.com',
       },
+      {
+        protocol: 'https',
+        hostname: 'supkem.co.ke',
+      },
+      {
+        protocol: 'https',
+        hostname: 'supkem.s3.eu-north-1.amazonaws.com',
+      },
+      {
+        protocol: 'http',
+        hostname: '127.0.0.1',
+      },
     ],
   },
 };
